eea3afe42faf82928305480832e0cfcd80488e49
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-04-28 13:46:17 +0200

    Create 2 rotte

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About</title>
+
+    <!-- IMPORT TAILWIND  -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <header>
+        PAGINA ABOUT
+    </header>
+    <main>
+        <a href="/">Torna alla home</a>
+    </main>
+</body>
+
+</html>
